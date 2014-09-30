17a42ef821463774969b5dcda707938cae62da79
ka_z_sdvx <gincleakazu3939@gmail.com>
2014-09-30 09:21:37 +0900

    Space Tab change

--- CGBasic/Lecture02.cpp
+++ CGBasic/Lecture02.cpp
@@ -19,29 +19,29 @@ const int SIZE_Y = 480;
 
 // 描画関数
 void Display(){
-	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
 }
 
 // 初期設定関数
 void Init(){
-	// 灰色
-	glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+    // 灰色
+    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
 }
 
 // メイン関数
 int main(int argc, char *argv[]){
-	// ■初期化
-	glutInit(&argc, argv);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(SIZE_X, SIZE_Y);
-	glutInitDisplayMode(GLUT_RGBA);
-	glutCreateWindow("CGBasic Subject02");
+    // ■初期化
+    glutInit(&argc, argv);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(SIZE_X, SIZE_Y);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutCreateWindow("CGBasic Subject02");
 
-	// ■メイン処理
-	glutDisplayFunc(Display);
-	Init();
-	glutMainLoop();
+    // ■メイン処理
+    glutDisplayFunc(Display);
+    Init();
+    glutMainLoop();
 
-	return 0;
+    return 0;
 }
--- CGBasic/MyOpenGL.cpp
+++ CGBasic/MyOpenGL.cpp
@@ -6,12 +6,12 @@ const int SIZE_Y = 480;
 
 struct Camera
 {
-	GLdouble EyeX = 0.0;
-	GLdouble EyeY = 0.0;
-	GLdouble EyeZ = 0.0;
-	GLdouble TarX = 0.0;
-	GLdouble TarY = 0.0;
-	GLdouble TarZ = 0.0;
+    GLdouble EyeX = 0.0;
+    GLdouble EyeY = 0.0;
+    GLdouble EyeZ = 0.0;
+    GLdouble TarX = 0.0;
+    GLdouble TarY = 0.0;
+    GLdouble TarZ = 0.0;
 };
 
 Camera camera;
@@ -24,64 +24,68 @@ void Reshape(int w, int h);
 void Timer(int v);
 
 void Init(){
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
 void Display(){
-	camera.EyeX = sin(3.14 * 2 / 360 * count) * 10;
-	camera.EyeY = sin(3.14 * 2 / 180 * count) * 10;
+    // ■計算
+    camera.EyeX = sin(3.14 * 2 / 360 * count) * 10;
+    camera.EyeY = sin(3.14 * 2 / 180 * count) * 10;
 
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	glLoadIdentity();
-	gluLookAt(
-		camera.EyeX, camera.EyeY, 5.0,
-		camera.TarX, camera.TarY, camera.TarZ,
-		0.0, 1.0, 0.0);
+    // カメラ設定
+    glLoadIdentity();
+    gluLookAt(
+        camera.EyeX, camera.EyeY, 5.0,
+        camera.TarX, camera.TarY, camera.TarZ,
+        0.0, 1.0, 0.0);
 
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.9f, 0.0f);
+    // ポリゴン描画
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.9f, 0.0f);
 
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(-0.9f, -0.9f, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-0.9f, -0.9f, 0.0f);
 
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex3f(0.9f, -0.9f, 0.0f);
-	glEnd();
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.9f, -0.9f, 0.0f);
+    glEnd();
 
-	glFlush();
+    glFlush();
 
-	count++;
+    // フレームカウント
+    count++;
 }
 
 void Reshape(int w, int h){
-	glViewport(0, 0, w, h);
+    glViewport(0, 0, w, h);
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
-	glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
 }
 
 void Timer(int v){
-	glutPostRedisplay();
-	glutTimerFunc(v, Timer, 30);
+    glutPostRedisplay();
+    glutTimerFunc(v, Timer, 15);
 }
 
 // -------------------- メイン関数 --------------------
 int main(int argc, char *argv[]){
-	glutInit(&argc, argv);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(SIZE_X, SIZE_Y);
-	glutInitDisplayMode(GLUT_RGBA);
-	glutCreateWindow("My OpenGL");
+    glutInit(&argc, argv);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(SIZE_X, SIZE_Y);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutCreateWindow("My OpenGL");
 
-	glutDisplayFunc(Display);
-	glutReshapeFunc(Reshape);
-	glutTimerFunc(0, Timer, 17);
-	Init();
-	glutMainLoop();
+    glutDisplayFunc(Display);
+    glutReshapeFunc(Reshape);
+    glutTimerFunc(0, Timer, 17);
+    Init();
+    glutMainLoop();
 
-	return 0;
+    return 0;
 }
